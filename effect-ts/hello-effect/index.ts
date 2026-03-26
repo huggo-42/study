5021002b5d345fcafb49216722c443b2e16f9da4
huggo-42 <victorhuggocontato42@gmail.com>
2026-03-25 23:33:14 -0300

    effect-ts/hello-effect: create project

---
+++ effect-ts/hello-effect/index.ts
@@ -0,0 +1,25 @@
+// import { Effect, Console } from "effect"
+//
+// const program = Console.log("Hello, World!")
+//
+// console.log("Hello via Bun!");
+//
+// Effect.runSync(program)
+
+
+import { Effect, Context } from "effect"
+
+class SomeContext extends Context.Tag("SomeContext")<SomeContext, {}>() {}
+
+// Assume we have an effect that succeeds with a number,
+// fails with an Error, and requires SomeContext
+declare const program: Effect.Effect<number, Error, SomeContext>
+
+// Extract the success type, which is number
+type A = Effect.Effect.Success<typeof program>
+
+// Extract the error type, which is Error
+type E = Effect.Effect.Error<typeof program>
+
+// Extract the context type, which is SomeContext
+type R = Effect.Effect.Context<typeof program>
