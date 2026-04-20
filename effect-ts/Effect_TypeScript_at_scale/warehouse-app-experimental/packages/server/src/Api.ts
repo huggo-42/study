471756da681e79ed6f1f51533d9f615fe3ee6792
huggo <victorhuggocontato42@gmail.com>
2026-04-19 23:15:28 -0300

    feat(effect-ts/etas): Effect Warehouse App, experiments, remove Todos related code

--- effect-ts/Effect_TypeScript_at_scale/warehouse-app-experimental/packages/server/src/Api.ts
+++ effect-ts/Effect_TypeScript_at_scale/warehouse-app-experimental/packages/server/src/Api.ts
@@ -1,21 +1,7 @@
 import { HttpApiBuilder } from "@effect/platform"
-import { TodosApi } from "@template/domain/TodosApi"
 import { Effect, Layer } from "effect"
-import { TodosRepository } from "./TodosRepository.js"
 import { Product, ProductApi } from "@template/domain/ProductApi"
 import * as Option from "effect/Option"
-
-
-const TodosApiLive = HttpApiBuilder.group(TodosApi, "todos", (handlers) =>
-	Effect.gen(function*() {
-		const todos = yield* TodosRepository
-		return handlers
-			.handle("getAllTodos", () => todos.getAll)
-			.handle("getTodoById", ({ path: { id } }) => todos.getById(id))
-			.handle("createTodo", ({ payload: { text } }) => todos.create(text))
-			.handle("completeTodo", ({ path: { id } }) => todos.complete(id))
-			.handle("removeTodo", ({ path: { id } }) => todos.remove(id))
-	}))
 
 const ProductApiLive = HttpApiBuilder.group(ProductApi, "products", (handlers) =>
 	handlers.handle("getAllProducts", () => Effect.succeed([
@@ -24,7 +10,6 @@ const ProductApiLive = HttpApiBuilder.group(ProductApi, "products", (handlers) =
 	]))
 )
 
-export const ApiLive = HttpApiBuilder.api(TodosApi).pipe(
-	Layer.provide(TodosApiLive),
+export const ApiLive = HttpApiBuilder.api(ProductApi).pipe(
 	Layer.provide(ProductApiLive)
 )
--- effect-ts/Effect_TypeScript_at_scale/warehouse-app-experimental/packages/server/src/server.ts
+++ effect-ts/Effect_TypeScript_at_scale/warehouse-app-experimental/packages/server/src/server.ts
@@ -3,11 +3,9 @@ import { NodeHttpServer, NodeRuntime } from "@effect/platform-node"
 import { Layer } from "effect"
 import { createServer } from "node:http"
 import { ApiLive } from "./Api.js"
-import { TodosRepository } from "./TodosRepository.js"
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
 	Layer.provide(ApiLive),
-	Layer.provide(TodosRepository.Default),
 	Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 }))
 )
 
